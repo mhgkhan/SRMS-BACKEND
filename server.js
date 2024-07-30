714c23ccc5136256f65e62266549fca1d194f1df
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-07-30 17:52:51 +0500

    Added Api route to for checking

--- server.js
+++ server.js
@@ -9,12 +9,24 @@ import express from "express"
 
 
 const app = express();
+app.use(express.json())
+
+
+
+
+
 
 app.use("/api/", (req,res,next)=>{
     console.log("api triggered")
     next();
 })
 
+app.get("/api/check", (req,res)=>{
+    res.status(200).json({
+        success:true,
+        message:"Congrats 🎉"
+    })
+})
 
 
 
