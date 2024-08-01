ef773bfd13867ae80e18c4356f69784f7eb58e61
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-08-01 17:17:00 +0500

    Added Checked If dbAction function is what returned

--- helpers/dbActions.js
+++ helpers/dbActions.js
@@ -19,6 +19,7 @@ export async function checkIfValueExistsInCollection(collection, field, value) {
   } catch (error) {
     return {
       error: true,
+      message: error.message,
       success: false,
       data: null,
     };
--- src/controllers/student/AuthController.js
+++ src/controllers/student/AuthController.js
@@ -1,27 +1,49 @@
 import { checkIfValueExistsInCollection } from "../../../helpers/dbActions.js";
-import { sendSuccessResponse } from "../../../helpers/UtilityFunctions.js";
+import {
+  sendErrorResponse,
+  sendSuccessResponse,
+} from "../../../helpers/UtilityFunctions.js";
 import StudentModel from "../../db/models/students/Student.js";
 
 class StudentAuthRouteController {
-    static handleStudentLogin = async (req,res) =>{
-        try {
+  static handleStudentLogin = async (req, res) => {
+    try {
+      const { stdId, stdPassword } = req.body;
+      // console.log(req.body)
+      // console.log(stdId, stdPassword)
 
-            const {stdId,stdPassword} = req.body;
-            // console.log(req.body)
-            // console.log(stdId, stdPassword)
+      const findStudent = await checkIfValueExistsInCollection(
+        StudentModel,
+        "stdId",
+        stdId
+      );
 
-            const findStudent = await checkIfValueExistsInCollection(StudentModel, "stdId", stdId)
+      // console.log(findStudent)
+      if (findStudent.error)
+        return sendErrorResponse(
+          res,
+          false,
+          "Server Error",
+          findStudent.message,
+          500
+        );
+      if (findStudent.success)
+        return sendErrorResponse(
+          res,
+          false,
+          "dublicate",
+          "Student of this id already exists",
+          400
+        );
 
-            console.log(findStudent)
-
-
-            sendSuccessResponse(res, true, "user logged success", {}, 201)
-
-            // return res.status(200).json({success:true, message:"hello world "})
-        } catch (error) {
+        if(!findStudent.success){
             
         }
-    }
+
+      return sendSuccessResponse(res, true, "user logged success", {}, 201);
+      // return res.status(200).json({success:true, message:"hello world "})
+    } catch (error) {}
+  };
 }
 
-export default StudentAuthRouteController
+export default StudentAuthRouteController;
