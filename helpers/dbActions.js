c77cbe790f5331e34ce9991d8c6640b2abd9d2f0
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-08-01 17:07:31 +0500

    removed comments

--- helpers/dbActions.js
+++ helpers/dbActions.js
@@ -1,38 +1,30 @@
-export async function checkIfValueExistsInCollection(collection, field,value) {
-    try {
-    
-        // console.log("runned")
+export async function checkIfValueExistsInCollection(collection, field, value) {
+  try {
 
-        // console.log(field, value)
+    const findData = await collection.findOne({ [field]: value });
 
 
-        const findData = await collection.findOne({[field]:value});
-
-        // console.log(findData)
-
-        if(findData == null || findData == undefined || findData == NaN) return{
-            error:false,
-            success:false,
-            data:findData
-        } 
-        else{
-            return{
-                error:false,
-                success:true,
-                data:findData
-
-
-            }
-        }
-
-        
-
-
-    } catch (error) {
-        return {
-            error:true,
-            success:false,
-            data: null
-        }
-    }
+    if (findData == null || findData == undefined || findData == NaN)
+      return {
+        error: false,
+        success: false,
+        data: findData,
+      };
+    else
+      return {
+        error: false,
+        success: true,
+        data: findData,
+      };
+  } catch (error) {
+    return {
+      error: true,
+      success: false,
+      data: null,
+    };
+  }
 }
+
+
+
+
