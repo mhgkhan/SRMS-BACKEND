99d09c26f33fbe71f2671c028067f9bc9af9f8e3
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-08-07 08:55:05 +0500

    Added Login to Header IN Responsive

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,23 +1,35 @@
-console.log("main js is working on site ")
+console.log("main js is working on site ");
 // setTimeout(() => {
 //     document.querySelector("body").classList.add("dark")
 // }, 5000);
 
+let openedHeader = false;
 
-let headerButton = document.querySelector(".header-button")
+function openHeader(e) {
+  if (openedHeader) {
+    openedHeader = false;
+    e.innerHTML = `<i class="fa-solid fa-bars"> </i>`;
+    document.getElementById("header").setAttribute("style","height:60px")
+  } else {
+    openedHeader = true;
+    e.innerHTML = `<i class="fa fa-close"> </i>`;
+    document.getElementById("header").setAttribute("style","height:310px;")
+  }
+}
 
+// let headerButton = document.querySelector(".header-button")
 
-headerButton.addEventListener("click", e=>{
+// headerButton.addEventListener("click", e=>{
 
-    if(headerButton.querySelector("i").classList.contains("fa-bars")){
-        headerButton.querySelector("i").classList.remove("fa-bars")
-        headerButton.querySelector("i").classList.add("fa-close")
-        console.log("close button showed")
-    }
-    else{
-        headerButton.querySelector("i").classList.add("fa-bars")
-        headerButton.querySelector("i").classList.remove("fa-close")
-        
-        console.log("menu button showted")
-    }
-})
+//     if(headerButton.querySelector("i").classList.contains("fa-bars")){
+//         headerButton.querySelector("i").classList.remove("fa-bars")
+//         headerButton.querySelector("i").classList.add("fa-close")
+//         console.log("close button showed")
+//     }
+//     else{
+//         headerButton.querySelector("i").classList.add("fa-bars")
+//         headerButton.querySelector("i").classList.remove("fa-close")
+
+//         console.log("menu button showted")
+//     }
+// })
