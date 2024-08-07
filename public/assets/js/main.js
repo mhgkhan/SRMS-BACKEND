2fe6832b57570a235b4f7fa7a4cb7e964a0a672f
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-08-07 08:42:14 +0500

    Header Responsives Completed

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,3 +2,22 @@ console.log("main js is working on site ")
 // setTimeout(() => {
 //     document.querySelector("body").classList.add("dark")
 // }, 5000);
+
+
+let headerButton = document.querySelector(".header-button")
+
+
+headerButton.addEventListener("click", e=>{
+
+    if(headerButton.querySelector("i").classList.contains("fa-bars")){
+        headerButton.querySelector("i").classList.remove("fa-bars")
+        headerButton.querySelector("i").classList.add("fa-close")
+        console.log("close button showed")
+    }
+    else{
+        headerButton.querySelector("i").classList.add("fa-bars")
+        headerButton.querySelector("i").classList.remove("fa-close")
+        
+        console.log("menu button showted")
+    }
+})
